114ccfee222452c296a44ce2e1a950306df54e3d
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-04-19 17:03:22 +0800

    Update app routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  resources :pengguna
   root 'beranda#index'
+
+  resources :pengguna
+  resources :sessions, only: [:new, :create, :destroy]
+
+  get 'mendaftar', to: 'pengguna#new', as: 'signup'
+  get 'masuk', to: 'sessions#new', as: 'login'
+  get 'keluar', to: 'sessions#destroy', as: 'logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
