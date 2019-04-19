dbb1b2f5dcb8f95116165959ad15044d6344cfee
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-04-19 16:55:08 +0800

    Create sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	pengguna = Pengguna.find_by_email(params[:email])
+    if pengguna && pengguna.authenticate(params[:password])
+      session[:pengguna_id] = pengguna.id
+      redirect_to root_url, notice: "Anda berhasil masuk."
+    else
+      flash.now[:alert] = "Email atau password tidak valid"
+      render "new"
+    end
   end
 
   def destroy
+  	session[:pengguna_id] = nil
+    redirect_to root_url, notice: "Anda telah keluar."
   end
 end
