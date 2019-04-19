319589d4ee9933c351b7f5a8aa0099d764deb50c
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-04-19 17:04:41 +0800

    current_user helper setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+	helper_method :current_user
+
+  def current_user
+    if session[:pengguna_id]
+      @current_user ||= Pengguna.find(session[:pengguna_id])
+    else
+      @current_user = nil
+    end
+  end
 end
