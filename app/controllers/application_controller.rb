845516521e84b26c0a3897321e6e99ebde0664eb
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-04-19 17:29:09 +0800

    Update current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     if session[:pengguna_id]
-      @current_user ||= Pengguna.find(session[:pengguna_id])
+      @current_user ||= Pengguna.find(session[:pengguna_id]) if session[:pengguna_id]
     else
       @current_user = nil
     end
