9403c9acb826048294a25ea156543601df10325c
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-04-20 20:11:41 +0800

    Cek Pengguna telah aktif atau belum

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,16 @@ class SessionsController < ApplicationController
   def create
   	pengguna = Pengguna.find_by_email(params[:session][:email].downcase)
     if pengguna && pengguna.authenticate(params[:session][:password])
-      log_in pengguna
-      params[:session][:remember_me] == '1' ? remember(pengguna) : forget(pengguna)
-      redirect_back_or pengguna
+      if pengguna.activated?
+        log_in pengguna
+        params[:session][:remember_me] == '1' ? remember(pengguna) : forget(pengguna)
+        redirect_back_or pengguna
+      else
+        message  = "Akun Anda belum diaktifkan. "
+        message += "Mohon periksa email Anda, klik pada tautan pada email yang kami kirim."
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = "Email atau password tidak valid"
       render "new"
