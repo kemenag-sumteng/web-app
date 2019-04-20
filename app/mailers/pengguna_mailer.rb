bb8bd3112cfc9ce959ab19afc57b37286a09c681
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-04-21 00:01:25 +0800

    Setup default email

--- app/mailers/pengguna_mailer.rb
+++ app/mailers/pengguna_mailer.rb
@@ -1,4 +1,5 @@
 class PenggunaMailer < ApplicationMailer
+  default :from => 'bimkat.kemenag.sumteng@gmail.com'
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
