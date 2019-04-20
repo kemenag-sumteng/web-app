d0af743904bea7a802498bd0ae2f6990b59b82d7
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-04-20 17:48:25 +0800

    Migrate AddAktivasiToPengguna

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_19_193527) do
+ActiveRecord::Schema.define(version: 2019_04_20_094441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 2019_04_19_193527) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "remember_digest"
+    t.string "aktivation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_pengguna_on_email", unique: true
   end
 
