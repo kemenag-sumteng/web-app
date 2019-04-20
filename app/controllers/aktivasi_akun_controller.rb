c0c464c73007cac3f9952ad9ef562e1900c479e2
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-04-20 20:07:36 +0800

    Update edit action AktivasiAkun

--- app/controllers/aktivasi_akun_controller.rb
+++ app/controllers/aktivasi_akun_controller.rb
@@ -1,2 +1,15 @@
 class AktivasiAkunController < ApplicationController
+	def edit
+    pengguna = Pengguna.find_by(email: params[:email])
+    if pengguna && !pengguna.activated? && pengguna.authenticated?(:activation, params[:id])
+      pengguna.update_attribute(:activated,    true)
+      pengguna.update_attribute(:activated_at, Time.zone.now)
+      log_in pengguna
+      flash[:success] = "Akun Anda telah diaktifkan!"
+      redirect_to pengguna
+    else
+      flash[:danger] = "Tautan aktivasi tidak valid"
+      redirect_to root_url
+    end
+  end
 end
