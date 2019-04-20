4d8f3c5fd64a47aae1ba4257c05a3c379503dd4d
Yosef Benny Widyokarsono <fybwid@gmail.com>
2019-04-20 18:18:34 +0800

    PenggunaMailer setup: - update account_activation method - update account_activation text and html

--- app/mailers/pengguna_mailer.rb
+++ app/mailers/pengguna_mailer.rb
@@ -5,10 +5,9 @@ class PenggunaMailer < ApplicationMailer
   #
   #   en.pengguna_mailer.account_activation.subject
   #
-  def account_activation
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def account_activation(pengguna)
+    @pengguna = pengguna
+    mail to: pengguna.email, subject: "Hai <%= @pengguna.nama_lengkap %>, ini tautan untuk mengaktivasi akun Anda di Aplikasi Web Bimas Katolik"
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
